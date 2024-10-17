1c4637080ac79527d0729de68c7613b978bcf3fc
FED <105217969+scn2930@users.noreply.github.com>
2024-10-17 16:16:53 +0900

    Fix : script 제거
    2024.10.17

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,10 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
-import { ServerStyleSheet } from "styled-components";
+import { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 const Document = () => {
   return (
     <Html lang="en">
-      <Head>
-        <script
-          src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"
-          defer
-        />
-        <Script
-          strategy="beforeInteractive"
-          src="/smartEditor/js/HuskyEZCreator.js"
-        ></Script>
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
@@ -29,8 +19,7 @@ Document.getInitialProps = async (ctx: any) => {
   try {
     ctx.renderPage = () =>
       originalRenderPage({
-        enhanceApp: (App: any) => (props: any) =>
-          sheet.collectStyles(<App {...props} />),
+        enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />),
       });
 
     const initialProps = await ctx.defaultGetInitialProps(ctx);
