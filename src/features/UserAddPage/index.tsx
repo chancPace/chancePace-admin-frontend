7a150a58beb1d992c437ddee370134f53e103837
FED <105217969+scn2930@users.noreply.github.com>
2024-10-20 22:18:02 +0900

    Feat : AddUser
    2024-10-20 관리자가 회원 등록하기 초안

--- src/features/UserAddPage/index.tsx
+++ src/features/UserAddPage/index.tsx
@@ -1,4 +1,53 @@
-const UserAddPage = () => {
-  return <>유저 등록 페이지</>;
+import React from 'react';
+import type { FormProps } from 'antd';
+import { Button, Form, Input } from 'antd';
+
+type FieldType = {
+  username?: string;
+  password?: string;
 };
+
+const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+  console.log('Success:', values);
+};
+
+const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
+
+const UserAddPage = () => (
+  <Form
+    name="basic"
+    labelCol={{ span: 8 }}
+    wrapperCol={{ span: 16 }}
+    style={{ maxWidth: 600 }}
+    initialValues={{ remember: true }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <Form.Item<FieldType>
+      label="아이디"
+      name="username"
+      rules={[{ required: true, message: 'Please input your username!' }]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item<FieldType>
+      label="비밀번호"
+      name="password"
+      rules={[{ required: true, message: 'Please input your password!' }]}
+    >
+      <Input.Password />
+    </Form.Item>
+
+    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
+);
+
 export default UserAddPage;
