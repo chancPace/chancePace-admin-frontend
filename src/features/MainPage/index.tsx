b2de52bef16e8c89c844c2ccdb50cb0ee580daf8
FED <105217969+scn2930@users.noreply.github.com>
2024-10-21 14:32:21 +0900

    Fix : css 수정
    2024-10-21 대시보드 css

--- src/features/MainPage/index.tsx
+++ src/features/MainPage/index.tsx
@@ -5,29 +5,35 @@ import personIcon from '@/assets/img/personIcon.png';
 const MainPage = () => {
   return (
     <MainStyled>
-      <div>
+      <div className="wrap top">
         <p>관리</p>
         <hr></hr>
-        <div>
-          공간 승인 요청 <span>2</span>
-        </div>
-        <div>
-          광고 승인 요청 <span>5</span>
-        </div>
-        <div>
-          문의 내역
-          <span>6</span>
+        <div className="content">
+          <p>
+            공간 승인 요청 <span>2</span>
+          </p>
+          <p>
+            광고 승인 요청 <span>5</span>
+          </p>
+          <p>
+            문의 내역
+            <span>6</span>
+          </p>
         </div>
       </div>
-      <div>
-        <p>금일 접속자 수</p>
-        <hr></hr>
-        <img src={personIcon.src} alt="personIcon" />
-        <div>공간 승인 요청</div>
-        <div>10</div>
-        <div>문의 내역</div>
+      <div className="bottom">
+        <div className="wrap bottomLeft">
+          <p>금일 접속자 수</p>
+          <hr></hr>
+          <div className="content">
+            <img src={personIcon.src} alt="personIcon" />
+            <span>10</span>
+          </div>
+        </div>
+        <div className="wrap bottomRight">
+          <Charts />
+        </div>
       </div>
-      <Charts />
     </MainStyled>
   );
 };
