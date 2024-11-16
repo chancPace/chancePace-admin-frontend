2fe0759b370bbc5c1a9cd3e6dfe607e29e530b12
FED <105217969+scn2930@users.noreply.github.com>
2024-11-16 21:01:37 +0900

    Fix : css 수정
    2024-11-16 매출 상세, 리뷰 상세 css 수정

--- src/features/ReviewDetailPage/style.ts
+++ src/features/ReviewDetailPage/style.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const ReviewDetailStyle = styled.div`
   p {
     font-size: ${({ theme }) => theme.fontSizes.xl};
-    margin: ${({ theme }) => theme.spacing.md};
+    margin-bottom: ${({ theme }) => theme.spacing.md};
     font-weight: bold;
   }
 `;
--- src/features/SalesDetailPage/style.ts
+++ src/features/SalesDetailPage/style.ts
@@ -3,15 +3,8 @@ import styled from 'styled-components';
 const SpaceDetailStyled = styled.div`
   p {
     font-size: ${({ theme }) => theme.fontSizes.xl};
-    margin: ${({ theme }) => theme.spacing.md};
+    margin-bottom: ${({ theme }) => theme.spacing.md};
     font-weight: bold;
-  }
-  .buttonWrap {
-    display: flex;
-    justify-content: space-between;
-  }
-  .edit {
-    margin: ${({ theme }) => theme.spacing.md} 0;
   }
 `;
 
