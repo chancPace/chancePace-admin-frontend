3afdca169b2a933bd8cebc4e081eae47b1f45046
FED <105217969+scn2930@users.noreply.github.com>
2024-11-19 00:52:39 +0900

    Fix : 회원가입시 체크박스, 아이디/비밀번호 찾기 제거
    2024-11-19 회원가입시 체크박스 제거, 로그인 페이지 아이디/비밀번호 찾기 제거

--- src/features/LoginPage/index.tsx
+++ src/features/LoginPage/index.tsx
@@ -92,9 +92,6 @@ const LoginPage = () => {
           <Link href="/signup" passHref>
             <span className="span1">아직 회원이 아니신가요?</span>
           </Link>
-          <Link href="/signup" passHref>
-            <span>아이디/비밀번호 찾기</span>
-          </Link>
         </div>
       </Form>
     </LoginStyled>
--- src/features/SignupPage/index.tsx
+++ src/features/SignupPage/index.tsx
@@ -1,104 +1,20 @@
 import { SignupFormStyled } from './styled';
 import { Form, message, FormInstance, Button } from 'antd';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 import InputField from '@/components/InputField';
-import CheckboxGroup from '@/components/CheckboxGroup';
 import { postSignup } from '@/pages/api/userApi';
 import { RuleObject } from 'antd/es/form';
-import { CheckboxChangeEvent } from 'antd/es/checkbox';
-import { AxiosError } from 'axios'; // AxiosError 타입을 import
+import { AxiosError } from 'axios';
 import { SignupData } from '@/types';
 
-interface CheckBoxItem {
-  value: string;
-  children: string;
-  checked: boolean;
-  required: boolean;
-}
-
 const SignupPage = () => {
-  const router = useRouter(); // useRouter 훅 가져오기
-  //체크박스의 정보를 담고 있는 상태 배열
-  const [smallCheckBoxs, setSmallCheckBoxs] = useState<CheckBoxItem[]>([
-    {
-      value: 'check1', //체크박스 고유값
-      children: '서비스 이용 약관(필수)', //체크박스 옆에 표시될 텍스트
-      checked: false, //체크박스가 선택되었는지
-      required: true, //해당 체크박스 선택이 필수인지 구분
-    },
-    {
-      value: 'check2',
-      children: '개인정보 수집 이용 (필수)',
-      checked: false,
-      required: true,
-    },
-    {
-      value: 'check3',
-      children: '마케팅 정보 수신 동의 (선택)',
-      checked: false,
-      required: false,
-    },
-  ]);
-
-  //모든 체크박스가 선택되었는지
-  const [isAllChecked, setIsAllChecked] = useState(false);
-  //필수 체크박스가 선택되지 않았을 경우의 에러 메세지
-  const [errorMessage, setErrorMessage] = useState<string | null>(null); // 에러 메시지 상태 추가
-
-  //전체 체크박스 선택
-  const onAllCheck = (e: CheckboxChangeEvent) => {
-    //체크박스 상태 가져오기
-    const checked = e.target.checked;
-    //체크박스의 상태 업데이트
-    setIsAllChecked(checked);
-    //모든 체크박스의 checked상태를 업데이트
-    setSmallCheckBoxs(smallCheckBoxs.map((x) => ({ ...x, checked: checked })));
-  };
-
-  //개별 체크박스 선택
-  const onSingleCheck = (e: CheckboxChangeEvent) => {
-    //클릭한 체크박스의 값을 저장
-    const targetValue = e.target.value;
-    setSmallCheckBoxs(
-      smallCheckBoxs.map((checkBox) =>
-        targetValue === checkBox.value
-          ? //해당 체크박스의 checked상태를 반전시킴
-            { ...checkBox, checked: !checkBox.checked }
-          : checkBox
-      )
-    );
-  };
-
-  //개별 체크박스가 변경될때마다 호출되어 모든 체크박스가 선택되면 전체 체크박스 상태를 업데이트
-  //every(): 배열에 모든 요소가 특정 조건을 만족하는지 확인될때 사용
-  useEffect(() => {
-    setIsAllChecked(smallCheckBoxs.every((x) => x.checked));
-  }, [smallCheckBoxs]);
-
+  const router = useRouter();
   const [duplicateError, setDuplicateError] = useState('');
-  //회원가입 처리 함수
-  //values-> 폼 필드의 값들
+
   const handleSignup = async (values: SignupData) => {
     const { email, password, adminSecretKey } = values;
-    const allChecked = smallCheckBoxs.every((checkbox) => {
-      if (checkbox.required) {
-        return checkbox.checked; // 필수 체크박스가 선택되어야 함
-      }
-      return true; // 선택이 필요 없는 체크박스는 무시
-    });
-
-    //필수 체크박스가 선택되지 않았을경우 에러 메세지
-    if (!allChecked) {
-      setErrorMessage('필수 항목을 동의해주세요');
-      message.error('필수 항목을 선택하세요.');
-      return;
-    }
-    //선택인 체크박스 중 하나라도 선택된게 있는지 확인
-    const isOptionalChecked = smallCheckBoxs
-      .filter((checkbox) => !checkbox.required)
-      .some((checkbox) => checkbox.checked);
-    const agreed = isOptionalChecked;
+    const agreed = true;
     const role = 'admin';
     try {
       const response = await postSignup({
@@ -124,8 +40,6 @@ const SignupPage = () => {
   const onFinishFailed = (errorInfo: any) => {
     // 유효성 검사 실패 시, message로 에러 메시지를 띄움
     if (errorInfo.errorFields.length > 0) {
-      // message.error('유효성 검사를 통과하지 못했습니다. 필드를 확인하세요.');
-
       // 각 필드별로도 오류 메시지를 표시할 수 있음
       message.error(errorInfo.errorFields[0].errors[0]);
     }
@@ -214,15 +128,6 @@ const SignupPage = () => {
           rules={[{ required: true, message: '관리자 키를 입력해주세요' }]}
           isPassword
         />
-        <Form.Item>
-          <CheckboxGroup
-            checkboxes={smallCheckBoxs}
-            onAllCheck={onAllCheck}
-            onSingleCheck={onSingleCheck}
-            isAllChecked={isAllChecked}
-          />
-          {errorMessage && <p className="error">{errorMessage}</p>}
-        </Form.Item>
         <Form.Item>
           <Button htmlType="submit">회원가입</Button>
         </Form.Item>
